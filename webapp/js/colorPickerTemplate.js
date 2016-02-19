b27c7c5039417eb31784370a583527250e3dd37c
Sebastian Kapunkt <Devbook@Sebastians-MBP.Speedport_W723_V_Typ_A_1_01_011>
2016-02-19 12:08:53 +0100

    moved directive js
    to colorPickerTemplate.js

---
+++ webapp/js/colorPickerTemplate.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Devbook on 2/19/16.
+ */
+colorPickerApp.directive('colorPicker', function () {
+    return {
+
+        scope: {
+            r: '@initR',
+            g: '@initG',
+            b: '@initB',
+            a: '@initA',
+            onChange: '&'
+        },
+        restrict: 'E',
+        templateUrl: 'directive/colorPickerTemplate.html', link: function (scope) {
+            var COLORS = ['r', 'g', 'b', 'a'];
+            COLORS.forEach(function (value) {
+                scope.$watch(value, function (newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        if (angular.isFunction(scope.onChange)) {
+                            scope.onChange(generateColorChangeObject());
+                        }
+                    }
+                });
+            });
+            var generateColorChangeObject = function () {
+                var obj = {};
+                COLORS.forEach(function (value) {
+                    obj[value] = scope[value];
+                });
+                return obj;
+            };
+        }
+    };
+});
